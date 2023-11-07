e0490b91e6344b26c5c4961d662010699eaad767
Neunapp <neunapp.servicios@gmail.com>
2023-11-07 12:17:26 -0500

    Create database.js

---
+++ database.js
@@ -0,0 +1,85 @@
+const listaPostulantes = [
+ {
+    'dni': '00000001',
+    'nombres': 'Jaime ',
+    'apellidos': 'Moyano',
+    'nacimiento': '2005-03-20',
+  },
+  {
+    'dni': '00000002',
+    'nombres': 'Marius ',
+    'apellidos': 'Vicente',
+    'nacimiento': '2002-01-15',
+  },
+  {
+    'dni': '00000003',
+    'nombres': 'Josep Maria',
+    'apellidos': 'Fidalgo',
+    'nacimiento': '1995-10-20',
+  },
+  {
+    'dni': '00000004',
+    'nombres': 'Adria ',
+    'apellidos': 'Carretero',
+    'nacimiento': '1999-10-15',
+  },
+  {
+    'dni': '00000005',
+    'nombres': 'Basilio',
+    'apellidos': 'Amador',
+    'nacimiento': '2001-12-05',
+  },
+]
+
+
+const listaEmpleados = [
+  {
+    'dni': '00000020',
+    'nombres': 'Maria Amparo',
+    'apellidos': 'Ballester',
+    'nacimiento': '12-05-2001',
+    'area': 'Informática',
+    'codigo': 'IN20020'
+  },
+  {
+    'dni': '00000021',
+    'nombres': 'Sheila ',
+    'apellidos': 'Mota',
+    'nacimiento': '12-05-2001',
+    'area': 'Contabilidad',
+    'codigo': 'CO20021'
+  },
+  {
+    'dni': '00000022',
+    'nombres': 'Mateo',
+    'apellidos': 'Rodríguez',
+    'nacimiento': '12-05-2001',
+    'area': 'Administracion',
+    'codigo': 'AD20022'
+  },
+  {
+    'dni': '00000023',
+    'nombres': 'Sofia',
+    'apellidos': 'Ramírez',
+    'nacimiento': '12-05-2001',
+    'area': 'Caja',
+    'codigo': 'CA20023'
+  },
+  {
+    'dni': '00000024',
+    'nombres': 'Maria Amparo',
+    'apellidos': 'Ballester',
+    'nacimiento': '12-05-2001',
+    'area': 'Informática',
+    'codigo': 'IN20020'
+  },
+  {
+    'dni': '00000025',
+    'nombres': 'Santiago ',
+    'apellidos': 'García',
+    'nacimiento': '12-05-2001',
+    'area': 'Contabilidad',
+    'codigo': 'CO20025'
+  },
+]
+export { listaPostulantes, listaEmpleados };
