1533e1359559ea669fd606fb976c9f451b3e8270
Neunapp <neunapp.servicios@gmail.com>
2023-11-07 12:14:56 -0500

    Create EliminarPostulante.vue

---
+++ EliminarPostulante.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="my-modal">
+    <div class="card is-centered primary">
+      <header class="card-header">
+        <p class="card-header-title">
+          ¿Rechazar Postulante?
+        </p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          Datos del postulante: 
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">SI</a>
+        <a href="#" class="card-footer-item">Cancelar</a>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
