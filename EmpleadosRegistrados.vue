75c1c5ff29ebf4207bceb9e2d0ab47a964d7fe6d
Neunapp <neunapp.servicios@gmail.com>
2023-11-08 01:56:57 -0500

    Create EmpleadosRegistrados.vue

---
+++ EmpleadosRegistrados.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="columns" style="width: 100%;">
+    <div class="column is-half">
+      <h3>Empleados Registrados</h3>
+      <div class="table-container">
+        <table class="table is-hoverable is-bordered">
+          <thead>
+            <th>#</th>
+            <th>Nombres</th>
+            <th>Apellidos</th>
+            <th>Codigo</th>
+            <th>*</th>
+          </thead>
+          <tbody>
+            <tr v-for="index in 10" :key="index">
+              <td>{{ index }}</td>
+              <td>Flor De Maria</td>
+              <td>Cantara Flores</td>
+              <td>IN1001</td>
+              <td>
+                <button class="button is-info is-light">Ver</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="column">
+      <h4>Detalle Empleados</h4>
+      <div class="box">
+        <article class="media">
+          <div class="media-left">
+            <figure class="image is-64x64">
+              <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>Nombres: </strong> <span>Flor De Maria</span>
+              </p>
+              <p>
+                <strong>Apellidos: </strong> <span>Cantara Flores</span>
+              </p>
+              <p>
+                <strong>Edad: </strong> <span>28 años</span>
+              </p>
+              <p>
+                <strong>DNI: </strong> <span>12345678</span>
+              </p>
+              <p>
+                <strong>Codigo: </strong> <span>IN1001</span>
+              </p>
+              <p>
+                <strong>Area: </strong> <span>Informatica</span>
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
