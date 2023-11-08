068d10d2d9276b5cc376633ef4959714be19a8ad
Neunapp <neunapp.servicios@gmail.com>
2023-11-08 17:11:18 -0500

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          Inicio  
+        </a>
+
+        <a class="navbar-item">
+          Postulantes
+        </a>
+        <a class="navbar-item">
+          Empleados
+        </a>
+      </div>
+    </div>
+  </nav>
+  <div class="columns is-centered" style="padding: 2em;">
+    <router-view/>
+  </div>
+  
+</template>
+
+<style lang="scss">
+.app {
+
+}
+</style>
