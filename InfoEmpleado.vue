c66dcf3296dc8fd3b84b74adf4320aade40b32b0
Neunapp <neunapp.servicios@gmail.com>
2023-12-06 10:57:56 -0500

    Create InfoEmpleado.vue

---
+++ InfoEmpleado.vue
@@ -0,0 +1,41 @@
+<template>
+  <article class="message">
+    <div class="message-header">
+      <p>Datos Empleado</p>
+    </div>
+    <div class="message-body">
+      <ul class="menu-list">
+        <li>
+          <a>
+            <span class="tag is-info" style="width: 60px; margin-right: 1em;">DNI:</span>
+            <span>12345678</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <span class="tag is-info" style="width: 60px; margin-right: 1em;">Nombres:</span>
+            <span>12345678</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <span class="tag is-info" style="width: 60px; margin-right: 1em;">Apellidos:</span>
+            <span>12345678</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <span class="tag is-info" style="width: 60px; margin-right: 1em;">Edad:</span>
+            <span>12345678</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <span class="tag is-info" style="width: 60px; margin-right: 1em;">Codigo:</span>
+            <span>12345678</span>
+          </a>
+        </li>
+    </ul>
+    </div>
+  </article>
+</template>
